85d41f4a42b604d18a6c4c7c6c3a0ee76b5f84aa
djembeman2 <52840818+djembeman2@users.noreply.github.com>
2023-11-20 21:44:55 -0600

    change the title to alice syays

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // It's a good idea to declare all your variables.
 // This will let our JS linter help us find 
 // spelling errors among other things.
-// Declaring with 'var' will make them global
+// Delaring with 'var' will make them global
 // variables.
 
 var aliceSays, bobSays, CarolSays, danSays, ellieSays, frankSays, harrySays, ivySays, jackSays;
@@ -14,8 +14,39 @@ let theMessage = "Kathy loves Larry";
 aliceSays = theMessage;
 bobSays = aliceSays;
 carolSays = bobSays;
+danSays = carolSays;
+ellieSays = danSays;
+frankSays = ellieSays;
+gregSays = frankSays;
+harrySays = gregSays;
+ivySays = harrySays;
+jackSays = ivySays;
 
+function sayMessages() {
+    let aliceSays, bobSays,
+    carolSays, danSays, ellieSays, gregSays,
+    frankSays, harrySays, ivySays, jackSays
+}
+aliceElm = document.querySelector("#alice-says");
+aliceElm.textContent = aliceSays;
 
-// call setNames from 'messages.js'
+bobElm = document.querySelector("#bob-says");
+bobElm.textContent = bobSays;
+
+let colorBtn;
+// select the button for tells-lynndale
+colorBtn = document.querySelector("#color-Swtich");
+// create the function to run onclick
+function tellcolor(){
+  aliceSays = "Sunny Sunday";
+ window.alert ("tellcolor")
+  aliceElm.textContent = aliceSays;
+  setMessages()
+}
+
+colorBtn.onclick =tellcolor
+/// callback function 
+// call setNames
+'messages.js'
 // to set the messages in the DOM.
 setMessages();
