2714f349bebe17b0157d2e02ed80f0c9f39286e8
djembeman2 <52840818+djembeman2@users.noreply.github.com>
2023-12-07 20:54:54 -0600

    Corrected the script

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+
 // It's a good idea to declare all your variables.
 // This will let our JS linter help us find 
 // spelling errors among other things.
@@ -9,7 +10,7 @@ var aliceSays, bobSays, CarolSays, danSays, ellieSays, frankSays, harrySays, ivy
 
 
 // this variable is set to a string.
-let theMessage = "Kathy loves Larry";
+let theMessage = "Kathy says What's Up my People";
 
 aliceSays = theMessage;
 bobSays = aliceSays;
@@ -22,31 +23,11 @@ harrySays = gregSays;
 ivySays = harrySays;
 jackSays = ivySays;
 
-function sayMessages() {
-    let aliceSays, bobSays,
-    carolSays, danSays, ellieSays, gregSays,
-    frankSays, harrySays, ivySays, jackSays
-}
-aliceElm = document.querySelector("#alice-says");
-aliceElm.textContent = aliceSays;
+// call setNames from
 
-bobElm = document.querySelector("#bob-says");
-bobElm.textContent = bobSays;
+//let aliceBtn;
+//aliceBtn = document.querySelector//////
+////("#color-Switch");
 
-let colorBtn;
-// select the button for tells-lynndale
-colorBtn = document.querySelector("#color-Swtich");
-// create the function to run onclick
-function tellcolor(){
-  aliceSays = "Sunny Sunday";
- window.alert ("tellcolor")
-  aliceElm.textContent = aliceSays;
-  setMessages()
-}
-
-colorBtn.onclick =tellcolor
-/// callback function 
-// call setNames
-'messages.js'
-// to set the messages in the DOM.
 setMessages();
+
